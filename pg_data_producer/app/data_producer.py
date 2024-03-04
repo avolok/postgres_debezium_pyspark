cfafda63c10b4ba6ba4e1bf102397c45f0974099
Alex Volok <olexandr.volok@asml.com>
2024-03-04 17:06:23 +0100

    refactor pg_data_producer

---
+++ pg_data_producer/app/data_producer.py
@@ -0,0 +1,60 @@
+import os
+from pathlib import Path
+import string
+from time import sleep
+from psycopg2 import connect
+import random
+from random import randrange
+from datetime import timedelta, date
+
+
+class DataProducer:
+    def __init__(self, database_configuration):
+        self.connection = connect(**database_configuration)
+        self.sleep_time: int = int(os.environ.get("SECONDS_BETWEEN_MODIFICATIONS", 10))
+
+    def run_script(self, script_path: str, query_parameters: tuple | None = None):
+        try:
+            cursor = self.connection.cursor()
+            sql_script_content = Path(script_path).read_text()
+            cursor.execute(sql_script_content, query_parameters)
+            self.connection.commit()
+        except Exception as e:
+            print(e)
+
+    def generate_id(self, size=6, chars=string.ascii_uppercase + string.digits):
+        return "".join(random.choice(chars) for _ in range(size))
+
+    def generate_date(self, start, end):
+        """
+        This function will return a random datetime between two datetime
+        objects.
+        """
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = randrange(int_delta)
+        return start + timedelta(seconds=random_second)
+
+    def launch(self):
+        ddl_script = "./sql/ddl.sql"
+        merge_script = "./sql/merge.sql"
+
+        # run initial ddl script
+        self.run_script(script_path="./sql/ddl.sql")
+        print("DDL script: ", ddl_script, "has been executed")
+
+        print("Starting the process with an interval in seconds:", self.sleep_time)
+
+        for i in range(10000):
+            random_int = random.randint(0, 100)
+            random_date = self.generate_date(
+                date.today() - timedelta(days=2000), date.today()
+            )
+            random_string = self.generate_id()
+
+            merge_parameters = (random_int, random_string, random_date)
+
+            self.run_script(script_path=merge_script, query_parameters=merge_parameters)
+            print(f"--> {merge_parameters}")
+
+            sleep(self.sleep_time)
---
+++ pg_data_producer/app/sql/merge.sql
@@ -0,0 +1,8 @@
+MERGE INTO cdctable t
+            USING (select %s as id, %s as name, cast(%s as date) as "date") s
+            ON s.id = t.id
+            WHEN MATCHED THEN
+            UPDATE SET name = s.name, "date" = s."date"
+            WHEN NOT MATCHED THEN
+            INSERT (id, name, "date")
+            VALUES (s.id, s.name, s."date");

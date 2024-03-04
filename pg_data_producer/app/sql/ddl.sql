d38b8e84d0d6ff8715bccc1a5e5fa272416500b6
Alex Volok <olexandr.volok@asml.com>
2024-03-04 17:07:14 +0100

    format sql

--- pg_data_producer/app/sql/ddl.sql
+++ pg_data_producer/app/sql/ddl.sql
@@ -3,9 +3,10 @@ CREATE TABLE IF NOT EXISTS cdcTable (
     name VARCHAR NOT NULL,
     date date not null
 );
+
 ALTER TABLE cdcTable REPLICA IDENTITY FULL;
-CREATE TABLE IF NOT EXISTS table1
-(
+
+CREATE TABLE IF NOT EXISTS table1 (
     col1 integer,
     col2 date,
     col3 character varying(50) COLLATE pg_catalog."default"
--- pg_data_producer/app/sql/merge.sql
+++ pg_data_producer/app/sql/merge.sql
@@ -1,8 +1,16 @@
-MERGE INTO cdctable t
-            USING (select %s as id, %s as name, cast(%s as date) as "date") s
-            ON s.id = t.id
-            WHEN MATCHED THEN
-            UPDATE SET name = s.name, "date" = s."date"
-            WHEN NOT MATCHED THEN
-            INSERT (id, name, "date")
-            VALUES (s.id, s.name, s."date");
+MERGE INTO cdctable t USING (
+    select
+        % s as id,
+        % s as name,
+        cast(% s as date) as "date"
+) s ON s.id = t.id
+WHEN MATCHED THEN
+UPDATE
+SET
+    name = s.name,
+    "date" = s."date"
+    WHEN NOT MATCHED THEN
+INSERT
+    (id, name, "date")
+VALUES
+    (s.id, s.name, s."date");
